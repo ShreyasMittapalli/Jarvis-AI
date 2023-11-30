fb51420f8e437662857530b55642c55c4c798b1a
ShreyasMittapalli <112803560+ShreyasMittapalli@users.noreply.github.com>
2023-11-30 21:08:44 +0530

    Add files via upload

---
+++ openaitest.py
@@ -0,0 +1,24 @@
+import os
+import openai
+import pyttsx3  
+from config import apikey
+
+openai.api_key = apikey
+
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Write an email to my boss for resignation?",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+text_result = response["choices"][0]["text"]
+
+print(text_result)
+
+engine = pyttsx3.init()
+engine.say(text_result)
+engine.runAndWait()
